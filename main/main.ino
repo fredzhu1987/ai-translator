6c96ca40efd6ed817961ac1c84899bc4510efc14
kuci <kuci@emeicang.com>
2025-06-16 23:14:46 +0800

    no message

--- main/main.ino
+++ main/main.ino
@@ -249,11 +249,8 @@ void initI2SMic() {
     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
     .communication_format = I2S_COMM_FORMAT_I2S_MSB,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 8,
-    .dma_buf_len = 1024,
-    .use_apll = false,
-    .tx_desc_auto_clear = false,
-    .fixed_mclk = 0
+    .dma_buf_count = 4,
+    .dma_buf_len = 512
   };
 
   const i2s_pin_config_t inmp441_pin_config = {
